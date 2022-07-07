b8c178b41a7056a1266207093047dd6996966a18
siddiqueath <MY_NAME@example.com>
2022-07-07 22:25:06 +0530

    from VS code initital

--- src/App.js
+++ src/App.js
@@ -1,25 +1,72 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import "./App.css";
+export default function App() {
+  const cards = [
+    {
+      name: "FREE",
+      price: "$0",
+      user: "✔ Single User",
+      storage: "✔ 5GB Storage",
+      publics: "✔ Unlimited publics publics",
+      access: "✔ Community Access",
+      privates: "✖ Unlimited privates publics",
+      support: "✖ support Phone Support",
+      subdomain: "✖ Free Subdomain",
+      report: "✖ report Status Reports"
+    },
+    {
+      name: "PLUS",
+      price: "$9",
+      user: "✔ 5 User",
+      storage: "✔ 50GB Storage",
+      publics: "✔ Unlimited publics publics",
+      access: "✔ Community Access",
+      privates: "✔ Unlimited privates publics",
+      support: "✔ support Phone Support",
+      subdomain: "✔ Free Subdomain",
+      report: "✖ report Status Reports"
+    },
+    {
+      name: "PRO",
+      price: "$49",
+      user: "✔ Unlimited Users",
+      storage: "✔ 150GB Storage",
+      publics: "✔ Unlimited publics publics",
+      access: "✔ Community Access",
+      privates: "✔ Unlimited privates publics",
+      support: "✔ support Phone Support",
+      subdomain: "✔ Free Subdomain",
+      report: "✔ report Status Reports"
+    }
+  ];
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="deck">
+      {cards.map((item, index) => (
+        <Deck card={item} key={index} />
+      ))}
     </div>
   );
 }
+//continue fromremoving div
+function Deck({ card }) {
+  return (
+    <div className="card">
+      <h4 className="title">{card.name}</h4>
+      <h5 className="price">
+        {card.price}
+        <span className="month">/month</span>
+      </h5>
 
-export default App;
+      <ul>
+        <li>{card.user}</li>
+        <li>{card.storage}</li>
+        <li>{card.publics}</li>
+        <li>{card.access}</li>
+        <li>{card.privates}</li>
+        <li>{card.support}</li>
+        <li>{card.subdomain}</li>
+        <li>{card.report}</li>
+      </ul>
+      <button className="btn">Button</button>
+    </div>
+  );
+}
